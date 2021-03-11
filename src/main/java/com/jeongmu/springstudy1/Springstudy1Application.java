4324f371cffe6ef8380080712f5ee1416cae81de
whwjdan <whwjdan@naver.com>
2021-03-11 19:04:54 +0900

    first commit

---
+++ src/main/java/com/jeongmu/springstudy1/Springstudy1Application.java
@@ -0,0 +1,13 @@
+package com.jeongmu.springstudy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springstudy1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springstudy1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeongmu/springstudy1/Springstudy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeongmu.springstudy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springstudy1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
