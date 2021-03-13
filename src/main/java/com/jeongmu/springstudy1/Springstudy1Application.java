a6f40c68b84d63fda301bc4c1bbd666d2a582f9a
whwjdan <whwjdan@naver.com>
2021-03-14 07:43:25 +0900

    jsaypt datasource 암호화 설정

--- src/main/java/com/jeongmu/springstudy1/Springstudy1Application.java
+++ src/main/java/com/jeongmu/springstudy1/Springstudy1Application.java
@@ -1,13 +1,16 @@
 package com.jeongmu.springstudy1;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class Springstudy1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Springstudy1Application.class, args);
     }
+
 
 }
--- src/test/java/com/jeongmu/springstudy1/Springstudy1ApplicationTests.java
+++ src/test/java/com/jeongmu/springstudy1/Springstudy1ApplicationTests.java
@@ -1,8 +1,10 @@
 package com.jeongmu.springstudy1;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@EnableEncryptableProperties
 @SpringBootTest
 public class Springstudy1ApplicationTests {
 
