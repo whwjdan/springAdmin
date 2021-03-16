fbe549572fc7b344c3b72bec03acfe29b8106f7e
whwjdan <whwjdan@naver.com>
2021-03-16 16:47:33 +0900

    all Repository Add

--- src/main/java/com/jeongmu/springstudy1/repository/AdminUserRepository.java
+++ src/main/java/com/jeongmu/springstudy1/repository/AdminUserRepository.java
@@ -1,13 +1,9 @@
 package com.jeongmu.springstudy1.repository;
 
-import com.jeongmu.springstudy1.model.entity.Category;
+import com.jeongmu.springstudy1.model.entity.AdminUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-
-
-
+public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
 }
--- src/main/java/com/jeongmu/springstudy1/repository/UserRepository.java
+++ src/main/java/com/jeongmu/springstudy1/repository/UserRepository.java
@@ -9,12 +9,22 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+
+
+
+
+
     // findBy까지 보고 select로 판별, Account, account를 매칭
     // select * from user where account = ? << test03, test04
 
+/*
     Optional<User> findByAccount(String account);
 
     Optional<User> findByEmail(String email);
     // select * from user where account = ? and email = ?
     Optional<User> findByAccountAndEmail(String account, String email);
+*/
+
+
+
 }
