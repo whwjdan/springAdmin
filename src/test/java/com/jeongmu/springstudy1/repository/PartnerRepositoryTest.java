3bbef78240779ddcf0529380c54d8dd1e337d5db
whwjdan <whwjdan@naver.com>
2021-03-23 13:42:21 +0900

    test: insert, read 테스트

--- src/test/java/com/jeongmu/springstudy1/repository/PartnerRepositoryTest.java
+++ src/test/java/com/jeongmu/springstudy1/repository/PartnerRepositoryTest.java
@@ -1,2 +1,53 @@
-package com.jeongmu.springstudy1.repository;public class PartnerRepositoryTest {
+package com.jeongmu.springstudy1.repository;
+
+import com.jeongmu.springstudy1.Springstudy1ApplicationTests;
+import com.jeongmu.springstudy1.model.entity.Partner;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class PartnerRepositoryTest extends Springstudy1ApplicationTests {
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Test
+    public void create(){
+        String name = "Partner01";
+        String status = "REGISTERED";
+        String address = "서울시 강남구";
+        String callCenter = "070-1111-2222";
+        String partnerNumber = "010-1111-2222";
+        String businessNumber = "1234567890123";
+        String ceoName ="홍길동";
+        LocalDateTime registeredAt = LocalDateTime.now();
+        LocalDateTime createdAt = LocalDateTime.now();
+        String createdBy = "AdminServer";
+        Long categoryId = 1L;
+
+        Partner partner = new Partner();
+        partner.setName(name);
+        partner.setStatus(status);
+        partner.setAddress(address);
+        partner.setCallCenter(callCenter);
+        partner.setPartnerNumber(partnerNumber);
+        partner.setBusinessNumber(businessNumber);
+        partner.setCeoName(ceoName);
+        partner.setRegisteredAt(registeredAt);
+        partner.setCreatedAt(createdAt);
+        partner.setCreatedBy(createdBy);
+        partner.setCategoryId(categoryId);
+
+        Partner newPartner = partnerRepository.save(partner);
+
+        Assert.assertNotNull(newPartner);
+    }
+
+    @Test
+    public void read(){
+
+    }
+
 }
