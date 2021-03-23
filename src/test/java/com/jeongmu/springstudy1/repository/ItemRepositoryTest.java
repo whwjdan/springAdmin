3e520e648c21437f270c8515eac67bf1d7feef03
whwjdan <whwjdan@naver.com>
2021-03-23 13:56:15 +0900

    test: item 테이블 insert, read 테스트

--- src/test/java/com/jeongmu/springstudy1/repository/ItemRepositoryTest.java
+++ src/test/java/com/jeongmu/springstudy1/repository/ItemRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class ItemRepositoryTest extends Springstudy1ApplicationTests {
@@ -16,10 +17,18 @@ public class ItemRepositoryTest extends Springstudy1ApplicationTests {
 
     @Test
     public void create(){
+
         Item item = new Item();
-        item.setName("노트북");
-        item.setPrice(100000);
-        item.setContent("삼성 노트북");
+        item.setStatus("UNREGISTERED");
+        item.setName("삼성컴퓨터");
+        item.setTitle("삼성컴퓨터 a100");
+        item.setContent("2019년형 노트북");
+        item.setPrice(900000);
+        item.setBrandName("삼성");
+        item.setRegisteredAt(LocalDateTime.now());
+        item.setCreatedAt(LocalDateTime.now());
+        item.setCreatedBy("Partner01");
+        item.setPartnerId(1L);
 
         Item newItem = itemRepository.save(item);
         Assert.assertNotNull(newItem);
