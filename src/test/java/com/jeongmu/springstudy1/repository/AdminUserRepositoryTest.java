408021d9227e5dc76062912f88b91c7b3737ac88
whwjdan <whwjdan@naver.com>
2021-03-24 14:24:37 +0900

    test: AdminUser insert, select Test

---
+++ src/test/java/com/jeongmu/springstudy1/repository/AdminUserRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.jeongmu.springstudy1.repository;
+
+
+import com.jeongmu.springstudy1.Springstudy1ApplicationTests;
+import com.jeongmu.springstudy1.model.entity.AdminUser;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class AdminUserRepositoryTest extends Springstudy1ApplicationTests {
+
+    @Autowired
+    private AdminUserRepository adminUserRepository;
+
+    @Test
+    public void create(){
+        AdminUser adminUser = new AdminUser();
+        adminUser.setAccount("AdminUser01");
+        adminUser.setPassword("AdminUser01");
+        adminUser.setStatus("REGISTERED");
+        adminUser.setRole("PARTNER");
+        adminUser.setCreatedAt(LocalDateTime.now());
+        adminUser.setCreatedBy("AdminServer");
+
+        AdminUser newAdminUser = adminUserRepository.save(adminUser);
+        Assert.assertNotNull(newAdminUser);
+    }
+
+}
