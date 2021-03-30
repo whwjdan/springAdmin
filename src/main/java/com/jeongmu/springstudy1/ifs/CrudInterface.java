3b9193a929c7f83c4a15f1163a529593505a3e0c
whwjdan <whwjdan@naver.com>
2021-03-30 16:43:49 +0900

    feat: CrudInterface 정의

--- src/main/java/com/jeongmu/springstudy1/ifs/CrudInterface.java
+++ src/main/java/com/jeongmu/springstudy1/ifs/CrudInterface.java
@@ -2,13 +2,13 @@ package com.jeongmu.springstudy1.ifs;
 
 import com.jeongmu.springstudy1.model.network.Header;
 
-public interface CrudInterface {
+public interface CrudInterface<Req,Res> {
+// <Req, Res> >> 리퀘스트, 리스폰스에 해당하는 제네릭
+    Header<Res> create(Header<Req> request);    // todo request object 추
 
-    Header create();    // todo request object 추
+    Header<Res> read(Long id);
 
-    Header read(Long id);
-
-    Header update();
+    Header<Res> update(Header<Req> request);
 
     Header delete(Long id);
 }
