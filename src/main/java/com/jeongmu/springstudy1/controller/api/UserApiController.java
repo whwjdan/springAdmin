ae9f57c91816ac9b73de44c17128ee7e2a260dcd
whwjdan <whwjdan@naver.com>
2021-03-30 17:40:22 +0900

    User Create Api 작성

--- src/main/java/com/jeongmu/springstudy1/controller/api/UserApiController.java
+++ src/main/java/com/jeongmu/springstudy1/controller/api/UserApiController.java
@@ -2,29 +2,51 @@ package com.jeongmu.springstudy1.controller.api;
 
 import com.jeongmu.springstudy1.ifs.CrudInterface;
 import com.jeongmu.springstudy1.model.network.Header;
+import com.jeongmu.springstudy1.model.network.request.UserApiRequest;
+import com.jeongmu.springstudy1.model.network.response.UserApiResponse;
+import com.jeongmu.springstudy1.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
+// Log >> 롬복에 포함된 로그
 @RestController
 @RequestMapping("/api/user")
 // /api/user의 주소를 가지는 컨트롤
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     // CRUD에 대해 작성
     @Override
     @PostMapping("") // ""으로 하면 /api/user로 매핑
-    public Header create(){
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request){
+
+        log.info("{}, {}", request, "ABC");
+        return userApiLogicService.create(request);
+
+    /*
+    Controller >> 클라이언트로부터 요청이 들어왔을 때 처리하는곳
+    Service >> 요청된 것을 수행 CRUD이므로
+    컨트롤러에서 @RequestBody로 클라이언트로부터
+    id, password 등 요청을 받으면
+    해당 리퀘스트를 userApiLogicService create 메소드를 호출하여
+    전달하고 서비스 단에서 JPA를 통해 DB에 저장
+
+     */
     }
 
     @Override
     @GetMapping("{id}") // /api/user/{id}
-    public Header read(@PathVariable(name ="id") Long id) {
+    public Header<UserApiResponse> read(@PathVariable(name ="id") Long id) {
         return null;
     }
 
     @Override
     @PutMapping("")
-    public Header update() {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> userApiRequest) {
         return null;
     }
 
--- src/main/java/com/jeongmu/springstudy1/service/UserApiLogicService.java
+++ src/main/java/com/jeongmu/springstudy1/service/UserApiLogicService.java
@@ -1,2 +1,90 @@
-package com.jeongmu.springstudy1.service;public class UserApiLogicService {
+package com.jeongmu.springstudy1.service;
+
+import com.jeongmu.springstudy1.ifs.CrudInterface;
+import com.jeongmu.springstudy1.model.entity.User;
+import com.jeongmu.springstudy1.model.network.Header;
+import com.jeongmu.springstudy1.model.network.request.UserApiRequest;
+import com.jeongmu.springstudy1.model.network.response.UserApiResponse;
+import com.jeongmu.springstudy1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // 1. request data
+    // 2. user 생성
+    // 3. 생성된 데이터 -> UserApiResponse return
+    @Override
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        // 1. request data 가져오기
+        UserApiRequest userApiRequest = request.getData();
+
+        // 2. User 생성
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        /*
+        User auser = new User();
+
+        auser.setAccount(~~);
+        auser.setPassword(~~);
+
+        위에 빌더 패턴은 User객체를 생성해서
+        변수를 지정해주는 것과 동일
+
+        서비스 로직이 개발되면 테스트 메소드를 통해 테스트 수행
+        */
+
+        User newUser = userRepository.save(user);
+
+        // 3. 생성된 데이터 -> userApiResponse return
+        return response(newUser);
+    }
+
+    @Override
+    public Header<UserApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<UserApiResponse> response(User user){
+        // user -> userApiResponse
+
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword()) // todo 암호화, 길이
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unRegisteredAt(user.getRegisteredAt())
+                .build();
+
+        // Header + data return
+
+        return Header.OK(userApiResponse);
+    }
 }
