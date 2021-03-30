883e4bea34eff79e3c2fcf921a4fb8132ab25aa2
whwjdan <whwjdan@naver.com>
2021-03-30 16:45:01 +0900

    UserApi Request, Response 설정

--- src/main/java/com/jeongmu/springstudy1/model/network/request/UserApiRequest.java
+++ src/main/java/com/jeongmu/springstudy1/model/network/request/UserApiRequest.java
@@ -1,2 +1,27 @@
-package com.jeongmu.springstudy1.model.network.request;public class UserApiRequest {
+package com.jeongmu.springstudy1.model.network.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserApiRequest {
+
+    private Long id;
+
+    private String account;
+
+    private String password;
+
+    private String status;
+
+    private String email;
+
+    private String phoneNumber;
+
+
 }
